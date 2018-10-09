c5be1f82ca83ea1cc16ae3cd73b1fda5950c9e79
saliouTest <cheikh-saliou.ndiaye@etudiant.univ-rennes1.fr>
2018-10-09 16:23:34 +0200

    feat: connexion/creation de compte

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class DefaultController extends Controller
 {
     /**
@@ -13,9 +15,55 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+
+    //  $user = $this->getDoctrine()->getManager()->getRepository('AppBundle:User')->getUtilisateur($this->getUser()->getUsername())
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+if($this->getUser() ===null)  {
+  return $this->render('default/index.html.twig'
+/*   [
+      'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+  ] */
+
+);
+
+}
+else {
+
+return $this->render('default/index2.html.twig',
+ array('user' => $this->getUser()->getName(),
+)
+);
+}
+
     }
+
+    /**
+     * @Route("/sucess", name="success")
+     */
+public function ConnexionReussieAction(){
+  return $this->render('default/index2.html.twig',
+   array('user' => $this->getUser()->getName(),
+  )
+  );
+}
+
+
+
+    /**
+     * @Route("/login", name="login")
+     */
+/*    public function loginAction(AuthenticationUtils $authenticationUtils)
+    {
+       return $this->render(
+           'auth/login.html.twig',
+           array(
+               'last_username' => $authenticationUtils->getLastUsername(),
+               'error'         => $authenticationUtils->getLastAuthenticationError(),
+           )
+       );
+    }*/
+
+
+
+
 }
---
+++ src/AppBundle/Controller/ListController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class ListController extends Controller
+{
+
+  /**
+   * @Route("/", name="welcome")
+   */
+/*    public function showAction(Request $request)
+    {
+        $characters = [
+        ];
+
+        return $this->render('auth/index.html.twig', array('character' => $characters));
+    }*/
+}
