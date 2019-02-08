e903caa3f53a49f648805efe0286eff8ddcc6ec2
Hugo Jové <hugo.jove2@gmail.com>
2019-02-08 21:18:32 +0100

    fix: feedback user et bouton retour carte

--- src/AppBundle/Controller/RegistrationController.php
+++ src/AppBundle/Controller/RegistrationController.php
@@ -23,41 +23,26 @@ class RegistrationController extends Controller
 
         if ($form->isSubmitted() && $form->isValid()) {
 
-           $data =$this->get('app.serialize')->entityToArray($form->getData());
+          $data =$this->get('app.serialize')->entityToArray($form->getData());
 
-           $mdp = $data['plainPassword'];
+          $mdp = $data['plainPassword'];
 
-           if( (strlen($mdp) <8) || (!(preg_match("~[0-9]~", $mdp))) || (!(preg_match("~[A-Z]~", $mdp)))   ) {
-             //echo ('votre mdp pas correct');die();
-             $this->addFlash('notice','Au moins 8 charactères dont 1 chiffre et 1 majuscule !');
-           }
-           else {
-             $response = $this->get('app.restclient')->post('api/users', $data);
-               if($response) {
-                  $this->addFlash('info','votre compte a été bien crée !');
-                  return $this->redirectToRoute('login');
-                }
-              else {
-                $this->addFlash('error','Erreur lors de la création de compte !');
-                return $this->render('auth/register.html.twig', [
-                    'form' => $form->createView()
-                ]);
-              }
-           }
-
-        /*   $response = $this->get('app.restclient')->post('api/users', $data);
-           if($response) {
-              $this->addFlash('info','votre compte a été bien crée !');
-              return $this->redirectToRoute('login');
-            }
-          else {
-            $this->addFlash('error','Erreur lors de la création de compte !');
-            return $this->render('auth/register.html.twig', [
-                'form' => $form->createView()
-            ]);
+          if( (strlen($mdp) <8) || (!(preg_match("~[0-9]~", $mdp))) || (!(preg_match("~[A-Z]~", $mdp)))){
+            $this->addFlash('error','Au moins 8 charactères dont 1 chiffre et 1 majuscule !');
           }
-          */
-
+          else {
+            $response = $this->get('app.restclient')->post('api/users', $data);
+              if($response) {
+                $this->addFlash('success','Votre compte a été bien créé !');
+                return $this->redirectToRoute('login');
+              }
+            else {
+              $this->addFlash('error','Erreur lors de la création de compte !');
+              return $this->render('auth/register.html.twig', [
+                  'form' => $form->createView()
+              ]);
+            }
+          }
         }
 
         return $this->render('auth/register.html.twig', [
